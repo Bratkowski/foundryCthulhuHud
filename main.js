b76aad3d4b3a82531406b769df99f19d1cef10a8
Aleksander Bratkowski <bratkowskirazdwa@gmail.com>
2025-12-07 19:53:24 +0100

    dodanie okna

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ Hooks.on("getSceneControlButtons", controls => {
   controls.notes.tools.CthulhuPlayerHud  = {
     name: "CthulhuPlayerHud",
     title: "Notes.PlayerHud",
-    icon: "fa-solid fa-wrench",
+    icon: "fa-solid fa-user",
     order: Object.keys(controls.tokens.tools).length,
     button: true,
     visible: true,
@@ -11,4 +11,41 @@ Hooks.on("getSceneControlButtons", controls => {
   console.log("Kliknięto HUD:", active);
 }
   };
-});
+})
+
+class CthulhuHud extends Application {
+    static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      id: "cthulhu-hud",
+      title: "Cthulhu HUD",
+      template: null,               // Na razie nie używamy osobnego HTML-a
+      popOut: true,                 // okno oderwane typu popup
+      width: 300,
+      height: "auto",
+      classes: ["cthulhu-hud-window"]
+    });
+  }
+
+  getData() {
+    return {
+      message: "HUD działa!"
+    };
+  }
+  
+  async _renderInner(data) {
+  const div = document.createElement("div");
+  div.innerHTML = `
+    div style="padding: 10px;">
+    <h2 style="margin:0; font-size: 20px;">Cthulhu HUD</h2>
+    <p>${data.message}</p>
+    </div>
+    `;
+  return div;
+  }
+
+  activateListeners(html) {
+  super.activateListeners(html);
+    // tu dodasz kliknięcia później
+  }
+}
+;
